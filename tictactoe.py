7f8878ae6bd3512bc2fd91eb1375371faf225def
Kenzie <kenzielynnstudio@gmail.com>
2022-02-12 13:48:39 -0500

    Define winning rows

--- tictactoe.py
+++ tictactoe.py
@@ -15,8 +15,24 @@ def get_player_move():
     col=int(col)
     board[row][col] = "X"
 
+
+def player_won() -> bool:
+    winning_row = ["X", "X", "X"]
+    if board[0] == winning_row:
+        return True
+    if board[1] == winning_row:
+        return True
+    if board[2] == winning_row:
+        return True
+        
+    return False
+    
+
 print("Welcome to tic tac toe.")
 
 while True:
     get_player_move()
     print_board()
+    if player_won():
+        print("You won!")
+        break
