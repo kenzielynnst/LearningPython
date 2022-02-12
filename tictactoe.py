f8bfe3427db6184af1d937be41a19187a772611b
Kenzie <kenzielynnstudio@gmail.com>
2022-02-12 13:30:59 -0500

    Get player moves

--- tictactoe.py
+++ tictactoe.py
@@ -8,13 +8,15 @@ def print_board():
     for row in board:
         print(" | ".join(row))
 
-print_board()
+def get_player_move():
+    move = input("Enter your move --> ")
+    row,col = move.split(",")
+    row=int(row)
+    col=int(col)
+    board[row][col] = "X"
 
 print("Welcome to tic tac toe.")
-move = input("Enter your move --> ")
-row,col = move.split(",")
-row=int(row)
-col=int(col)
-board[row][col] = "X"
 
-print_board()
+while True:
+    get_player_move()
+    print_board()
